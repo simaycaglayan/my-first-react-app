ac0a206d388e3df48616fb03c5c7d68404c9341d
simayll <simaycaglayan3@gmail.com>
2019-06-17 15:06:46 +0300

    third commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import MyComponent from './components/MyComponent'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           simay
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="nooper noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <MyComponent name="Sara" age="25" > this is children </MyComponent>
     </div>
-  );
-}
+    );
+    }
+
 
 export default App;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,9 +1,25 @@
 import React, {Component} from 'react';
 class MyComponent extends Component {
+    state = {
+    value: 0
+    };
+    incrementValue = () => {
+        console.log("increment value clicked");
+        let value = this.state.value;
+        value = value + 1;
+        let newState = {value: value};
+        this.setState(newState);
+    };
+
     render(){
         return (
             <div>
                 this is my component
+                {this.props.name}
+                and my age is {this.props.age}
+                and --- {this.props.children} ---
+                and my value is {this.state.value}
+                <button onClick={ () => this.incrementValue()}>increment value </button>
             </div>
         );
     }
